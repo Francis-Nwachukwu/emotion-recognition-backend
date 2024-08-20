173ecdccc0cbebc4dcaa9580b778ee5709a8ca36
Francis Nwachukwu <francisnwachukwu100@gmail.com>
2024-08-20 14:33:31 +0100

    added type declaration

--- src/typeDeclaration.d.ts
+++ src/typeDeclaration.d.ts
@@ -1,6 +1,6 @@
 declare module "mongoose-extend-schema";
-// declare module "cors";
-// declare module "bcrypt";
-// declare module "jsonwebtoken";
-// declare module "multer";
-// declare module "express";
+declare module "cors";
+declare module "bcrypt";
+declare module "jsonwebtoken";
+declare module "multer";
+declare module "express";
